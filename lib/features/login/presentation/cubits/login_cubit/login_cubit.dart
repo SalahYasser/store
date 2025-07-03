006d39c016269024dabf2b15a479c6b4b6258314
salahyasserallaithy <salahy.allaithy@gmail.com>
2025-07-03 19:07:04 +0300

    implement login cubit

--- lib/features/login/presentation/cubits/login_cubit/login_cubit.dart
+++ lib/features/login/presentation/cubits/login_cubit/login_cubit.dart
@@ -1,8 +1,19 @@
 import 'package:bloc/bloc.dart';
+import 'package:dio/dio.dart';
 import 'package:meta/meta.dart';
 
 part 'login_state.dart';
 
 class LoginCubit extends Cubit<LoginState> {
   LoginCubit() : super(LoginInitial());
+
+  final Dio dio = Dio();
+
+  login({required String name, required String password}) async {
+    final response = await dio.post(
+      'https://dummyjson.com/auth/login',
+      data: {"username": name, "password": password},
+      options: Options(headers: {'Content-Type': 'application/json'}),
+    );
+  }
 }

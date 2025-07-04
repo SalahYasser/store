8a8815f17769f0c22021df36acaed4d053d7d883
salahyasserallaithy <salahy.allaithy@gmail.com>
2025-07-04 17:37:21 +0300

    implement shared preferences to login method

--- lib/features/login/presentation/cubits/login_cubit/login_cubit.dart
+++ lib/features/login/presentation/cubits/login_cubit/login_cubit.dart
@@ -1,6 +1,7 @@
 import 'package:bloc/bloc.dart';
 import 'package:dio/dio.dart';
 import 'package:meta/meta.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 part 'login_state.dart';
 
@@ -9,11 +10,31 @@ class LoginCubit extends Cubit<LoginState> {
 
   final Dio dio = Dio();
 
-  login({required String name, required String password}) async {
-    final response = await dio.post(
-      'https://dummyjson.com/auth/login',
-      data: {"username": name, "password": password},
-      options: Options(headers: {'Content-Type': 'application/json'}),
-    );
+  Future<void> login({required String name, required String password}) async {
+    try {
+      final response = await dio.post(
+        'https://dummyjson.com/auth/login',
+        data: {"username": name, "password": password},
+        options: Options(headers: {'Content-Type': 'application/json'}),
+      );
+
+      if (response.statusCode == 200) {
+        final data = response.data;
+
+        final accessToken = data['accessToken'];
+
+        final pref = await SharedPreferences.getInstance();
+
+        await pref.setString('accessToken', accessToken);
+
+        print('Saved token: $accessToken');
+        print('Login successful. Access token saved.');
+      } else {
+        print('Login failed with status: ${response.statusCode}');
+      }
+
+    } on Exception catch (e) {
+      print('login error: $e');
+    }
   }
 }
